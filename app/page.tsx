ebb730f808d614a86265eece9b5ac08975085d43
Akshat <codepreneur.in@gmail.com>
2025-09-26 15:55:20 +0530

    ui

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,9 @@
 import { Keypair } from "@solana/web3.js";
 import { generateMnemonic ,mnemonicToSeedSync} from "bip39";
 import { derivePath } from "ed25519-hd-key";
+import { Badge } from "@/components/ui/badge"
 import nacl from "tweetnacl";
+import { Switch } from "@/components/ui/switch"
 
 async function key(){
 
@@ -28,7 +30,24 @@ for (let i = 0; i < 4; i++) {
 export default function Home() {
   return (
     <>
+    <div className="flex flex-col">
+      <div className="flex flex-row">
+
+
+    <div className=" w-full h-12 text-4xl m-6 ml-36">
+     Kosh
+     <Badge variant="outline" className="h-5 min-w-5 rounded-full px-1 font-mono tabular-nums align-text-top" >v1.3</Badge>
+    </div>
+    <div>
+      <Switch className="m-6 mr-36 "/>
+      </div>
+
+   
+
+    
+    </div>
     <button onClick={key} className="p-3">Generate Keys</button>
+    </div>
     </>
   );
 }
